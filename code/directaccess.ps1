50943dc1e6b7c1763941cce4ce4425d9e60ff630
brandonweeks <brandonweeks@users.noreply.github.com>
2021-05-10 18:47:57 +0000

    deploy: 90ebdd67e665b6b952a5fd8d63a1a6cee60fa6d7

--- code/directaccess.ps1
+++ code/directaccess.ps1
@@ -1,42 +1,73 @@
-$domain = $(Get-ADDomain).DNSRoot
-$gpo = 'DirectAccess Server Settings'
-$ruleName = 'DirectAccess Policy-DaServerToCorp'
-$algorithms = @("RSA", "ECDSA256", "ECDSA384")
+function Set-NetIPsecPhase1AuthSetAlgorithms {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [System.String]
+        $Domain = $(Get-ADDomain).DNSRoot,
+    
+        [System.String]
+        $GPO = [string] 'DirectAccess Server Settings',
+    
+        [System.String]
+        $RuleName = 'DirectAccess Policy-DaServerToCorp',
+    
+        [Parameter(mandatory = $true)]
+        [System.String]
+        $Authority,
+    
+        [ValidateSet("ECDSA256", "ECDSA384", "RSA")]
+        [System.String[]]
+        $Algorithm = @("RSA"),
+    
+        [Parameter(Mandatory = $false)]
+        [switch]
+        $Force
+    )
 
-#Sanity check that the GPO exists.
-Get-GPO -Name $gpo -Domain $domain -ErrorAction Stop | Out-Null
-
-$rule = Get-NetIPsecRule -DisplayName $ruleName -PolicyStore "$domain\$gpo" -ErrorAction Stop 
-$authSet = $rule | Get-NetIPsecPhase1AuthSet -ErrorAction Stop
-
-
-# Collect proposals into a hash by Authority and then by Signing. There should
-# only proposal per authority and signing combination, otherwise the exisiting
-# proposals are too complex for this script to reason about.
-$proposalsByAuthority = @{}
-foreach ($proposal in $authSet.Proposal) {
-    if ($proposal.AuthenticationMethod -ne "MachineCert") {
-        Write-Host 'Only supported Authentication Method is MachineCert'
-        return
+    try {
+        #Sanity check that the GPO exists.
+        Get-GPO -Name $GPO -Domain $Domain | Out-Null
     }
-    if ($proposalsByAuthority.ContainsKey($proposal.Authority)) {
-        if ($proposalsByAuthority[$proposal.Authority].ContainsKey($proposal.Signing)) {
-            Write-Host 'Only one proposal proposal per Authority and Signing combination is supported'
+    catch {
+        throw
+    }
+
+    $rule = Get-NetIPsecRule -DisplayName $RuleName -PolicyStore "$domain\$gpo"
+    $authSet = $rule | Get-NetIPsecPhase1AuthSet
+
+    $proposalForAuthority = @()
+    foreach ($p in $authSet.Proposal) {
+        if ($p.Authority -ne $Authority) {
+            continue
+        }
+        if ($p.AuthenticationMethod -ne "MachineCert") {
+            Write-Error 'Only the MachineCert Authentication Method is supported'
             return
         }
-        $proposalsByAuthority[$proposal.Authority][$proposal.Signing] = $proposal
-    } else {
-        $proposalsByAuthority[$proposal.Authority] = @{$proposal.Signing = $proposal}
+        $proposalForAuthority += $p
     }
-}
 
-$proposals = New-Object Collections.Generic.List[CimInstance]
-foreach ($v in $proposalsByAuthority.Values) {
-    foreach ($alg in $algorithms) {
-        $proposal = $($v.Values)[0].Clone()
-        $proposal.Signing = $alg
+    if ((-not $Force) -and $proposalForAuthority.Count -gt 1) {
+        Write-Error  "More than 1 proposal already exists for the authority, pass -Force to overwrite."
+        return
+    }
+
+    $proposals = New-Object Collections.Generic.List[CimInstance]
+    foreach ($alg in $Algorithm) {
+        # If there is more than one proposal for the given authority, the
+        # first proposal is used as the template. If the exisiting proposals
+        # differ by more than the Signing field this could cause unintended 
+        # results.
+        $proposal = $proposalForAuthority[0].Clone()
+        try {
+            $proposal.Signing = $alg
+        }
+        catch [System.Management.Automation.SetValueInvocationException] {
+            throw
+        }
         $proposals.Add($proposal)
     }
+
+    $authSet | Set-NetIPsecPhase1AuthSet -Proposal $proposals -PassThru -WhatIf:$WhatIfPreference 
 }
 
-$authSet | Set-NetIPsecPhase1AuthSet -Proposal $proposals
+Export-ModuleMember -Function Set-NetIPsecPhase1AuthSetAlgorithms
